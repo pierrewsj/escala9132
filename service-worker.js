a13329bd3edbb3dd6a75816962f62557c23fb438
pierrewsj <pierrewenderson1@gmail.com>
2026-04-28 11:23:40 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'escala-9132-v4';
+const FILES = ['./', './index.html', './style.css', './script.js', './dados.js', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
